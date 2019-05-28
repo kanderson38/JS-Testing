b45ce1ea89b56251710f2ba683553ebd01a21430
Kirsten Anderson <kirsten@kirstenmunson.com>
2019-05-27 21:40:00 -0700

    all tests pass, a couple new tests written

--- spec/is_pangram.spec.js
+++ spec/is_pangram.spec.js
@@ -10,54 +10,74 @@ describe('isPangram()', () => {
     const text = 'the quick brown fox jumps over the lazy dog';
 
     // Act
+    let result = isPangram(text);
 
     // Assert
+    expect(result).toBe(true);
   });
 
   test('works with "abcdefghijklmnopqrstuvwxyz"', () => {
     // Arrange
+    const text = "abcdefghijklmnopqrstuvwxyz";
 
     // Act
-
+    let result = isPangram(text);
     // Assert
-
+    expect(result).toBe(true);
   });
 
   test("missing character 'x'", () => {
     // Arrange
-
+    const text = "abcdefghijklmnopqrstuvwyz";
     // Act
-
+    let result = isPangram(text);
     // Assert
-
+    expect(result).toBe(false);
   });
 
   test('empty sentence', () => {
     // Arrange
-
+    const text = "";
     // Act
-
+    const result = isPangram(text);
     // Assert
-
+    expect(result).toBe(false);
   });
 
   test('pangram with underscores instead of spaces works', () => {
     // Arrange
-
+    const text = 'the_quick_brown_fox_jumps_over_the_lazy_dog';
     // Act
-
+    const result = isPangram(text);
     // Assert
-
+    expect(result).toBe(true);
   });
 
   test('pangram with numbers', () => {
     // Arrange
-
+    const text = 'the quick brown fox jumps over the lazy dog 38';
     // Act
-
+    const result = isPangram(text);
     // Assert
-
+    expect(result).toBe(true);
   });
 
-  // Write your own test case
+  test('26 letters but missing Y', () => {
+    const text = 'the quick brown fox jumps over the laz dog';
+
+    const result = isPangram(text);
+
+    expect(result).toBe(false);
+  })
+
+  test('uppercase', () => {
+    let text = 'the QUICK brown fox jumps over the lazy dog';
+
+    
+
+
+    const result = isPangram(text);
+
+    expect(result).toBe(true);
+  })
 });
--- src/is_pangram.js
+++ src/is_pangram.js
@@ -1,5 +1,21 @@
 const isPangram = function isPangram(text) {
+    if (text.length < 26) {
+        return false;
+    }
 
+    let allLetters = 'abcdefghijklmnopqrstuvwxyz';
+    text = text.toLowerCase();
+    for (let i = 0; i <= text.length; i += 1){
+        if (allLetters.includes(text.charAt(i))) {
+            allLetters = allLetters.split(text.charAt(i)).join('');
+        } 
+    }
+
+    if (allLetters.length == 0) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 module.exports = isPangram;
